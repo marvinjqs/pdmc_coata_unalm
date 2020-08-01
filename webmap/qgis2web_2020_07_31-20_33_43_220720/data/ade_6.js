b028d6a1198d84d3a20f01cbf22c3ba52062b914
Marvin J. Quispe Sedano <20141056@lamolina.edu.pe>
2020-07-31 20:55:31 -0500

    Update webmap

---
+++ webmap/qgis2web_2020_07_31-20_33_43_220720/data/ade_6.js
@@ -0,0 +1,8 @@
+var json_ade_6 = {
+"type": "FeatureCollection",
+"name": "ade_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -71.079332445041771, -15.079896758498082 ], [ -69.841062489157466, -15.087878039946606 ], [ -69.84483648955252, -16.015315830936974 ], [ -71.086260142379942, -16.006842475347703 ], [ -71.079332445041771, -15.079896758498082 ] ] ] ] } }
+]
+}
